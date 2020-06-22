d0df077a73178d0eb1df647dd3ecf6cc5b497e05
Piotr Ferensztajn <Piotr_Ferensztajn@MCKINSEY.COM>
2020-06-22 22:37:25 +0200

    Moved the rest of side effects to sagas

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,13 +4,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA_r60oD2lnizQpfp8zEJc2GD2QpFdCeM4",
-    authDomain: "to-do-list-f03d0.firebaseapp.com",
-    databaseURL: "https://to-do-list-f03d0.firebaseio.com",
-    projectId: "to-do-list-f03d0",
-    storageBucket: "to-do-list-f03d0.appspot.com",
-    messagingSenderId: "1009474257004",
-    appId: "1:1009474257004:web:2053d39393f91f0d8c1849"
+
   };
 
   firebase.initializeApp(firebaseConfig);
