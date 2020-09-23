eda0ac13eb705c824d4600e6f8eddb43be416cbd
Piotr Ferensztajn <Piotr_Ferensztajn@MCKINSEY.COM>
2020-09-23 22:42:15 +0200

    wired up mongo atlas connection

--- server/models/index.model.js
+++ server/models/index.model.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 
-mongoose.connect("mongodb://localhost:27017/todos", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-
-// mongoose.connect(process.env.MONGO_URL, {
+// mongoose.connect("mongodb://localhost:27017/todos", {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 //   useFindAndModify: false,
 // });
+
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 
 mongoose.Promise = Promise;
