b83fd13d07b50d9988f3e1bba7673363c559dc88
Piotr Ferensztajn <Piotr_Ferensztajn@MCKINSEY.COM>
2020-10-03 20:59:54 +0200

    finished tests for filter-bar redux (actions, reducer, sagas)

--- src/redux/filter-bar/filter-bar.sagas.test.js
+++ src/redux/filter-bar/filter-bar.sagas.test.js
@@ -6,46 +6,143 @@ import { FilterBarTypes } from "./filter-bar.types";
 import {
   showFilterPreviewSuccess,
   showFilterPreviewFailure,
+  setPreviewLoading,
+  clearFilterPreview,
+  fetchToDoSSuccess,
+  fetchToDoSFailure,
+  fetchFilteredToDosMainInputSuccess,
+  fetchFilteredToDosMainInputFailure,
 } from "./filter-bar.actions";
 
+import { asyncActionStart } from "../todo-lists-container/todo-lists-container.actions";
+
+import { MAIN_INPUT_ID } from "../../constants/constants";
+
 const setup = async (expectedResult) => {
-  const mockFetch = (expectedResult) => (...args) =>
+  jest.spyOn(global, "fetch").mockImplementation(() =>
     Promise.resolve({
       json: () => Promise.resolve(expectedResult),
-    });
+    })
+  );
+};
 
-  const fetch = mockFetch(expectedResult);
-  global.fetch = fetch;
-
-  const resObject = await fetch(null);
-  const jsonPromise = resObject.json();
-  const jsonData = await resObject.json();
-
-  return {
-    resObject,
-    jsonPromise,
-    jsonData,
-  };
+const setupError = async (errorMessage) => {
+  jest.spyOn(global, "fetch").mockImplementation(() => {
+    throw Error(errorMessage);
+  });
 };
 
 describe("FilterBar sagas", () => {
-  it("fetchPreview should get data from API and call success action", async () => {
-    const expectedPreview = ["todo1", "todo2", "todo3"];
-    const { resObject, jsonPromise, jsonData } = await setup(expectedPreview);
-    const action = {
-      type: FilterBarTypes.FETCH_FILTER_PREVIEW_START,
-      payload: {
-        word: "test",
-        filters: [],
-      },
+  it("fetchPreview should call setPreviewLoading action, get data from API and call success action", async () => {
+    expect.assertions(4);
+
+    const listID = 123;
+    const value = "data";
+
+    const action = { payload: { listID, filters: [], word: "test" } };
+    const expectedResult = {
+      listID,
+      value,
     };
+
+    setup(expectedResult);
+    const res = await fetch();
+    const data = await res.json();
 
     const gen = sagas.fetchPreview(action);
 
-    expect(gen.next().value).toEqual(fetch("whatever"));
-    expect(gen.next(resObject).value).toEqual(jsonPromise);
-    expect(gen.next(jsonData).value).toEqual(
-      put(showFilterPreviewSuccess(jsonData))
+    expect(gen.next().value).toEqual(put(setPreviewLoading(listID)));
+    expect(gen.next().value).toEqual(fetch());
+    expect(gen.next(res).value).toEqual(res.json());
+    expect(gen.next(data).value).toEqual(
+      put(showFilterPreviewSuccess({ listID, data }))
     );
+  });
+
+  it("fetchPreview should call setPreviewLoading action, get data from API and call failure action on error", async () => {
+    expect.assertions(2);
+
+    const listID = 123;
+    const action = { payload: { listID, filters: [], word: "test" } };
+
+    setupError("Server error");
+    const gen = sagas.fetchPreview(action);
+
+    expect(gen.next().value).toEqual(put(setPreviewLoading(listID)));
+
+    try {
+      await fetch();
+    } catch (error) {
+      expect(gen.next().value).toEqual(
+        put(showFilterPreviewFailure({ listID, error }))
+      );
+    }
+  });
+
+  it("fetchFilteredToDos should call asyncActionStart, clearFilterPreview, get data from api and call success action", async () => {
+    expect.assertions(6);
+
+    const listID = 456;
+    const filters = [];
+    const sorts = [];
+    const action = { payload: { listID, filters, sorts } };
+    const expectedResult = {
+      data: "this is data",
+      filters,
+      sorts,
+    };
+
+    setup(expectedResult);
+
+    const res = await fetch();
+    const data = await res.json();
+
+    const gen = sagas.fetchFilteredToDos(action);
+
+    expect(gen.next().value).toEqual(put(asyncActionStart()));
+    expect(gen.next().value).toEqual(put(clearFilterPreview(listID)));
+    expect(gen.next().value).toEqual(fetch());
+    expect(gen.next(res).value).toEqual(res.json());
+    expect(gen.next(data).value).toEqual(
+      put(fetchToDoSSuccess({ listID, todos: data, filters, sorts }))
+    );
+
+    const action2 = { payload: { listID: MAIN_INPUT_ID, filters, sorts } };
+
+    const gen2 = sagas.fetchFilteredToDos(action2);
+
+    gen2.next();
+    gen2.next();
+    gen2.next();
+    gen2.next(res);
+    expect(gen2.next(data).value).toEqual(
+      put(fetchFilteredToDosMainInputSuccess({ data, filters }))
+    );
+  });
+
+  it("fetchFilteredToDos should call asyncActionStart, clearFilterPreview, get data from api and call failure action on error", async () => {
+    const action = { payload: { listID: 456 } };
+    const action2 = { payload: { listID: MAIN_INPUT_ID } };
+
+    setupError("Server error");
+
+    const gen = sagas.fetchFilteredToDos(action);
+    const gen2 = sagas.fetchFilteredToDos(action2);
+
+    expect(gen.next().value).toEqual(put(asyncActionStart()));
+    expect(gen2.next().value).toEqual(put(asyncActionStart()));
+    expect(gen.next().value).toEqual(put(clearFilterPreview(456)));
+    expect(gen2.next().value).toEqual(put(clearFilterPreview(MAIN_INPUT_ID)));
+
+    try {
+      await fetch();
+    } catch (error) {
+      expect(gen.next().value).toEqual(
+        put(fetchToDoSFailure({ listID: 456, error }))
+      );
+      expect(gen2.next().value).toEqual(
+        put(fetchFilteredToDosMainInputFailure({ error }))
+      );
+    }
   });
 });
